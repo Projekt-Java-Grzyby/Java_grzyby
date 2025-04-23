fde1689d256bc0b53ff21b2068fb270daa00fe91
Łukasz Matecki <UL0275834@edu.uni.lodz.pl>
2025-04-23 19:13:31 +0200

    Commit Main

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,56 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.sql.*;
+import java.util.Properties;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-        //hi
+        //ZMienic ścieżkę na swoją + zmienić ścieżkę w bazie danych
+        String url = "jdbc:h2:file:D:/Java_grzyby/src/database/grzyby";
+        Properties prop = new Properties();
+        prop.setProperty("user", "");
+        prop.setProperty("password", "");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        try (Connection connection = DriverManager.getConnection(url, prop)) {
+            System.out.println("Database connection: " + connection.isValid(0));
+
+            // SELECT
+            try (PreparedStatement preparedStatement1 = connection.prepareStatement("SELECT * FROM grzyb WHERE id > ?")) {
+                preparedStatement1.setInt(1, 0);
+                try (ResultSet resultSet = preparedStatement1.executeQuery()) {
+                    while (resultSet.next()) {
+                        System.out.println(resultSet.getInt("id") + " " + resultSet.getString("nazwa") + " ");
+                    }
+                }
+            }
+
+//            // INSERT
+//            try (PreparedStatement preparedStatement2 = connection.prepareStatement("INSERT INTO grzyb (nazwa, nazwa_powszechna, opis) VALUES (?, ?)");) {
+//                preparedStatement2.setString(1, "Borowik Szlachetny");
+//                preparedStatement2.setString(2, "Prawdziwek");
+//                preparedStatement2.setString(3, "To grzybek");
+//                int rowsInserted = preparedStatement2.executeUpdate();
+//                System.out.println("Number of rows inserted: " + rowsInserted);
+//            }
+//
+//            // UPDATE
+//            try (PreparedStatement preparedStatement3 = connection.prepareStatement("UPDATE grzyb SET nazwa_powszechna = ? WHERE id = ?")) {
+//                preparedStatement3.setString(1, "Prawdziwek");
+//                preparedStatement3.setInt(2, 14);
+//                int rowsUpdated = preparedStatement3.executeUpdate();
+//                System.out.println("Number of rows updated: " + rowsUpdated);
+//            }
+//
+//            // DELETE
+//            try (PreparedStatement preparedStatement4 = connection.prepareStatement("DELETE FROM grzyb WHERE id > ?")) {
+//                preparedStatement4.setInt(1, 9);
+//                int rowsDeleted = preparedStatement4.executeUpdate();
+//                System.out.println("Number of rows deleted: " + rowsDeleted);
+//            }
+
+            catch (SQLException e) {
+                e.printStackTrace();
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
         }
     }
 }
